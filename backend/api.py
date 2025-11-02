36562d715881d8aae959370d4882c915fba44d88
Sarah <sarahbwood15@gmail.com>
2025-11-02 15:45:15 +0200

    Add patch route

--- backend/api.py
+++ backend/api.py
@@ -1,4 +1,3 @@
-import time
 import os
 import psycopg2
 from flask import Flask, render_template, request, url_for, redirect, jsonify
@@ -17,25 +16,65 @@ def connect_to_db():
 
 @app.get('/api/todos')
 def get_todos():
-    conn = connect_to_db()
-    cur = conn.cursor()
-    cur.execute('SELECT row_to_json(t) FROM (SELECT id, title, completed, created_at, user_id FROM todos) t') 
-    todoList = cur.fetchall()
-    cur.close()
-    conn.close()
-    return todoList
+    try:
+        conn = connect_to_db()
+        cur = conn.cursor()
+        cur.execute('SELECT row_to_json(t) FROM (SELECT id, title, completed, created_at, user_id FROM todos) t') 
+        todoList = cur.fetchall()
+        
+        return todoList
+    
+    except psycopg2.Error as e:
+        return jsonify({'error' : 'A database error occurred: {e}'}), 500
+    
+    except Exception as e:
+        return jsonify({'error' : 'An error occurred: {e}'}), 500
+
+    finally:
+        cur.close()
+        conn.close()
 
 @app.post('/api/todos')
 def todo_post():
     title = request.form['title']
     completed = request.form['completed']
-    # created_at = request.form['created_at']
     user_id = request.form['user_id']
 
-    conn = connect_to_db()
-    cur = conn.cursor()
-    cur.execute('INSERT INTO todos (title, completed, created_at, user_id) VALUES (%s, %s, CURRENT_TIMESTAMP, %s)', (title, completed, user_id))
-    conn.commit()
-    cur.close()
-    conn.close()
-    return jsonify({"message": "ToDo posted successfully"}), 200
+    try:
+        conn = connect_to_db()
+        cur = conn.cursor()
+        cur.execute('INSERT INTO todos (title, completed, created_at, user_id) VALUES (%s, %s, CURRENT_TIMESTAMP, %s)', (title, completed, user_id))
+        conn.commit()
+        return jsonify({'message': 'ToDo posted successfully.'}), 201
+    
+    except psycopg2.Error as e:
+        return jsonify({'error' : 'A database error occurred: {e}'}), 500
+    
+    except Exception as e:
+        return jsonify({'error' : 'An error occurred: {e}'}), 500
+    
+    finally:
+        cur.close()
+        conn.close()
+    
+@app.patch('/api/todos/<id>')
+def todo_patch(id):
+    title = request.form['title']
+    completed = request.form['completed']
+
+    try:
+        conn = connect_to_db()
+        cur = conn.cursor()
+        cur.execute('UPDATE todos SET title = %s, completed = %s WHERE id = %s', (title, completed, id))
+        conn.commit()
+        return jsonify({'message': 'ToDo updated successfully.'}), 200
+    
+    except psycopg2.Error as e:
+        return jsonify({'error' : 'A database error occurred: {e}'}), 500
+    
+    except Exception as e:
+        return jsonify({'error' : 'An error occurred: {e}'}), 500
+    
+    finally:
+        cur.close()
+        conn.close()
