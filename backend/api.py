91cd369040c2e7ba48eb336fbb7354225026ee1c
Sarah <sarahbwood15@gmail.com>
2025-11-09 17:55:11 +0200

    Add user registration route

--- backend/api.py
+++ backend/api.py
@@ -1,8 +1,10 @@
 import os
 import psycopg2
 from flask import Flask, render_template, request, url_for, redirect, jsonify
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
+bcrypt = Bcrypt(app)
 
 def connect_to_db():
     conn = psycopg2.connect(
@@ -13,6 +15,29 @@ def connect_to_db():
     )
 
     return conn
+@app.post('/api/register')
+def register_user():
+    username = request.form['username']
+    password = request.form['password']
+    
+    password_hash = bcrypt.generate_password_hash(password)
+
+    try:
+        conn = connect_to_db()
+        cur = conn.cursor()
+        cur.execute('INSERT INTO users (username, password) VALUES (%s, %s)', (username, password_hash))
+        conn.commit()
+        return jsonify({'message': 'User registered successfully.'}), 201
+    
+    except psycopg2.Error as e:
+        return jsonify({'error' : f'A database error occurred: {e}'}), 500
+    
+    except Exception as e:
+        return jsonify({'error' : f'An error occurred: {e}'}), 500
+    
+    finally:
+        cur.close()
+        conn.close()
 
 @app.get('/api/todos')
 def get_todos():
@@ -25,17 +50,17 @@ def get_todos():
         return todoList
     
     except psycopg2.Error as e:
-        return jsonify({'error' : 'A database error occurred: {e}'}), 500
+        return jsonify({'error' : f'A database error occurred: {e}'}), 500
     
     except Exception as e:
-        return jsonify({'error' : 'An error occurred: {e}'}), 500
+        return jsonify({'error' : f'An error occurred: {e}'}), 500
 
     finally:
         cur.close()
         conn.close()
 
 @app.post('/api/todos')
-def todo_post():
+def post_post():
     title = request.form['title']
     completed = request.form['completed']
     user_id = request.form['user_id']
@@ -48,17 +73,17 @@ def todo_post():
         return jsonify({'message': 'ToDo posted successfully.'}), 201
     
     except psycopg2.Error as e:
-        return jsonify({'error' : 'A database error occurred: {e}'}), 500
+        return jsonify({'error' : f'A database error occurred: {e}'}), 500
     
     except Exception as e:
-        return jsonify({'error' : 'An error occurred: {e}'}), 500
+        return jsonify({'error' : f'An error occurred: {e}'}), 500
     
     finally:
         cur.close()
         conn.close()
     
 @app.patch('/api/todos/<id>')
-def todo_patch(id):
+def patch_todo(id):
     title = request.form['title']
     completed = request.form['completed']
 
@@ -70,17 +95,17 @@ def todo_patch(id):
         return jsonify({'message': 'ToDo updated successfully.'}), 200
     
     except psycopg2.Error as e:
-        return jsonify({'error' : 'A database error occurred: {e}'}), 500
+        return jsonify({'error' : f'A database error occurred: {e}'}), 500
     
     except Exception as e:
-        return jsonify({'error' : 'An error occurred: {e}'}), 500
+        return jsonify({'error' : f'An error occurred: {e}'}), 500
     
     finally:
         cur.close()
         conn.close()
 
 @app.delete('/api/todos/<id>')
-def todo_delete(id):
+def delete_todo(id):
     try:
         conn = connect_to_db()
         cur = conn.cursor()
@@ -89,10 +114,10 @@ def todo_delete(id):
         return jsonify({'message': 'ToDo deleted successfully.'}), 200
     
     except psycopg2.Error as e:
-        return jsonify({'error' : 'A database error occurred: {e}'}), 500
+        return jsonify({'error' : f'A database error occurred: {e}'}), 500
     
     except Exception as e:
-        return jsonify({'error' : 'An error occurred: {e}'}), 500
+        return jsonify({'error' : f'An error occurred: {e}'}), 500
     
     finally:
         cur.close()
