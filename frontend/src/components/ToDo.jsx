9b19a626c57cee534e1693a578b5e7fd7974cc1d
Sarah <sarahbwood15@gmail.com>
2026-01-18 19:31:14 +0200

    Implement in-line editing for todos

--- frontend/src/components/ToDo.jsx
+++ frontend/src/components/ToDo.jsx
@@ -1,5 +1,11 @@
 import { useState } from "react";
-import { ListItem, ListItemText, IconButton, Checkbox } from "@mui/material";
+import {
+  ListItem,
+  ListItemText,
+  IconButton,
+  Checkbox,
+  TextField,
+} from "@mui/material";
 import { DeleteOutlined, EditOutlined } from "@mui/icons-material";
 import axios from "axios";
 import Swal from "sweetalert2";
@@ -11,6 +17,7 @@ function ToDo(props) {
     completed: props.completed,
     id: props.id,
   });
+  const [editInline, setEditInline] = useState(false);
 
   axios.defaults.withCredentials = true;
   axios.defaults.xsrfCookieName = "csrf_access_token";
@@ -117,9 +124,57 @@ function ToDo(props) {
     });
   }
 
+  function updateTitle(e) {
+    setTodo((prevTodo) => {
+      return {
+        ...prevTodo,
+        title: e.target.value,
+      };
+    });
+  }
+
+  function enableInlineEditing() {
+    setEditInline(true);
+  }
+
+  function editTodoInline() {
+    axios
+      .patch(
+        `api/todos/${todo.id}`,
+        {
+          title: todo.title,
+          completed: todo.completed,
+        },
+        {
+          headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+          },
+        }
+      )
+      .then((response) => {
+        console.log(response);
+        if (response.status === 200) {
+          props.refreshTodoList();
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+    setEditInline(false);
+  }
+
   return (
     <ListItem>
-      <ListItemText primary={todo.title} />
+      {editInline ? (
+        <TextField
+          value={todo.title}
+          onChange={updateTitle}
+          onBlur={editTodoInline}
+          variant="filled"
+        />
+      ) : (
+        <ListItemText primary={todo.title} onClick={enableInlineEditing} />
+      )}
       <Checkbox checked={todo.completed} onChange={updateCompletedStatus} />
       <IconButton edge="end" onClick={editTodo}>
         <EditOutlined />
