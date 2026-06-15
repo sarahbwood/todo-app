ee0cfeea78ee9e0a639828d8247714746d0506b1
Sarah <sarahbwood15@gmail.com>
2026-06-15 18:04:35 +0200

    Add important files

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,19 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   server: {
-    host: '0.0.0.0',
+    host: "0.0.0.0",
     port: 3000,
-    allowedHosts: ['localhost', 'host.docker.internal'],
+    allowedHosts: ["localhost", "host.docker.internal"],
     proxy: {
-      '/api': {
-        target: 'http://backend:8000', 
+      "/api": {
+        target: "http://backend:8000",
         changeOrigin: true,
         secure: false,
       },
     },
   },
-})
+});
